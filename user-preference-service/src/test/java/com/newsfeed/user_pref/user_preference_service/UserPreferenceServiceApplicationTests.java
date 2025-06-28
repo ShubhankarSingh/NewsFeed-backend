6ccb30f0db4a2672b9a27ca91ead2c22d7384a50
Shubhankar Singh <singhshubhankar01@gmail.com>
2025-06-28 16:22:37 +0530

    Initial commit - backend setup

---
+++ user-preference-service/src/test/java/com/newsfeed/user_pref/user_preference_service/UserPreferenceServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.newsfeed.user_pref.user_preference_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserPreferenceServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
